41cd5f2f5e9ca99b6f0e5df5d0984a4ac4bee8b7
Riley <vishnukey@gmail.com>
2020-02-25 00:17:05 -0700

    initial commit

---
+++ Dictionary.md
@@ -0,0 +1,14 @@
+# Dictionary Of Common Programming Terms
+
+## Framework
+
+a prebuilt system used for dealing with a particular task in a programming language, such as making a Graphical User Interface or building as server
+
+## Graphical User Interface (GUI)
+
+The thing that people look at when they think of an application
+
+## Java
+
+A multi-purpose programming built around Object Oriented Design
+
---
+++ mkDict.py
@@ -0,0 +1,59 @@
+#!/usr/bin/env python
+
+import sys
+import os
+from glob import glob
+
+DEBUG = True
+
+def debug(msg):
+    if DEBUG:
+        print("[INFO] ", msg)
+
+EXTS = {
+        "definition" : "def",
+        "template" : "template"
+        }
+
+DEF_PATH = "Definitions"
+TEMPLATE_PATH = "Templates"
+
+def getDefs(root = DEF_PATH, ext = EXTS['definition']):
+    definitions = []
+    for fname in glob(os.path.join(root, "*.%s" % ext)):
+        with open(fname, 'r') as f:
+            data = list(map(lambda s: s.replace("\n", ''), f))
+            title = data[0:1][0]
+            body = "".join(data[2:])
+            definitions.append((title, body, fname))
+    definitions.sort(key = lambda x: x[0])
+    return definitions
+
+
+def getTemplate(fname, root = TEMPLATE_PATH, ext = EXTS['template']):
+    with open(os.path.join(root, "%s.%s" % (fname, ext)), 'r') as f:
+        return f.read()
+
+def substitue(template):
+    def inner(definition):
+        title, body, _ = definition
+        return template.format(term=title, defintion=body)
+    return inner
+
+def build(defintions):
+    return "\n".join(defintions)
+
+def genDict(body, template):
+    return template.format(defs=body)
+
+def mkDict(body, oname):
+    with open(oname, 'w') as f:
+        f.write(body)
+
+defs = getDefs()
+defTemplate = getTemplate('definition')
+dictTemplate = getTemplate('dictionary')
+dictionary = build(map(substitue(defTemplate), defs))
+mkDict(genDict(dictionary, dictTemplate), "Dictionary.md")
+
+
